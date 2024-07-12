0efc1f0951d1a1e3181a6f7411c2b896ef7f859b
Michail Borisenko <bmaa@yandex.ru>
2024-07-12 13:03:41 +0300

    bma 5 step 2 create db ok v last

---
+++ dbt/dbtools.go
@@ -0,0 +1,82 @@
+/*
+	Пакет - каталог
+
+во всех файлах *.go внутри пакета-каталога объявляем pakage <имя каталога>
+package "dbt"
+имена файлов не важны
+импортируются пакеты import "github.com/<github user name>/<project name>/<packege name>"
+import "github.com/bmaayandexru/go_final_project/dbt"
+пускаем обязательно из модуля main
+изменения подхватятся автоматически
+заливать предварительно на гитхаб ничего не надо
+*/
+package dbt
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "modernc.org/sqlite"
+
+	"github.com/bmaayandexru/go_final_project/tests"
+)
+
+// см bmadraft.md
+var schemaSQL string = `
+CREATE TABLE scheduler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date CHAR(8) NOT NULL DEFAULT "20000101", 
+    title VARCHAR(32) NOT NULL DEFAULT "",
+    comment TEXT NOT NULL DEFAULT "",
+    repeat VARCHAR(128) NOT NULL DEFAULT ""
+);
+CREATE INDEX idx_date ON scheduler (date); 
+CREATE UNIQUE INDEX idx_title ON scheduler (title); 
+`
+
+var sqlDB *sql.DB
+
+func InitDBase() {
+	fmt.Println("Init Data Base...")
+	fmt.Println("settings DBFile", tests.DBFile)
+	envDBFile := os.Getenv("TODO_DBFILE")
+	fmt.Println("enviroment DBFile ", envDBFile)
+	if envDBFile == "" {
+		envDBFile = tests.DBFile
+	}
+	fmt.Println("Result DBFile ", envDBFile)
+	// лог схемы
+	// fmt.Println(schemaSQL)
+	/*
+		appPath, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbFile := filepath.Join(filepath.Dir(appPath), "scheduler.db")
+	*/
+	_, err := os.Stat(envDBFile)
+	var install bool
+	/*
+		if err != nil {
+			install = true
+		}
+	*/
+	install = (err != nil)
+	fmt.Println("Need install ", install)
+	// если install равен true, после открытия БД требуется выполнить
+	// sql-запрос с CREATE TABLE и CREATE INDEX
+	sqlDB, err = sql.Open("sqlite", envDBFile)
+	if err != nil {
+		fmt.Println("bma err:", err)
+		return
+	}
+	if install {
+		// нужно создать таблицу, т к файла не было
+		if _, err = sqlDB.Exec(schemaSQL); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	// defer sqlDB.Close()
+}
--- main.go
+++ main.go
@@ -5,40 +5,54 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/bmaayandexru/go_final_project/db"
+	"github.com/bmaayandexru/go_final_project/dbt"
 	"github.com/bmaayandexru/go_final_project/tests"
 )
 
 var mux *http.ServeMux
 
 func main() {
-	db.InitDBase()
+	dbt.InitDBase()
 	// лог-контроль
 	fmt.Println("Запускаем сервер")
 	mux = http.NewServeMux()
-	// вешаем обработчик
+	// вешаем отладочный обработчик
 	mux.HandleFunc("/m", mainHandle)
-	//	webDir := "web/"
-	//	mux.Handle("/", http.FileServer(http.Dir(webDir)))
 	// запуск файлового сервера в подкаталоге web
 	mux.Handle("/", http.FileServer(http.Dir("web/")))
-	// переменая tests.Port из settings.go
-	// !!! заменить на переменную окружения TODO_PORT с помощью os.Getenv(key string) string
-	// порт из settings.go
-	settingsStrPort := fmt.Sprintf(":%d", tests.Port)
-	// порт из переменной окружения. задание со *
-	envStrPort := os.Getenv("TODO_PORT")
-	// лог-контроль
-	fmt.Printf("envPort *%s* settingdStrPort *%s* \n", envStrPort, settingsStrPort)
-	fmt.Println("Set port from enviroment...")
-	err := http.ListenAndServe(envStrPort, mux)
-
+	// определение порта прослушки
+	strPort := defStrPort()
+	err := http.ListenAndServe(strPort, mux)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Завершаем работу")
 }
 
+func defStrPort() string {
+	defPort := "7540"
+	// переменая tests.Port из settings.go
+	settingsStrPort := fmt.Sprintf("%d", tests.Port)
+	// лог-контроль
+	fmt.Printf("settingdStrPort *%s* \n", settingsStrPort)
+	if settingsStrPort != "" {
+		// значение не пустое. переобределяем
+		defPort = settingsStrPort
+	}
+	// порт из переменной окружения TODO_PORT. задание со *
+	envStrPort := os.Getenv("TODO_PORT")
+	// лог-контроль
+	fmt.Printf("envPort *%s* \n", envStrPort)
+	if envStrPort != "" {
+		// значение не пустое. переобределяем
+		defPort = envStrPort
+	}
+	// итоговое значение
+	fmt.Printf("Set port %s \n", defPort)
+	return ":" + defPort
+}
+
+// отладочный обработчик. убрать
 func mainHandle(res http.ResponseWriter, req *http.Request) {
 	// лог-контроль
 	fmt.Println("Получен запрос")
@@ -48,6 +62,4 @@ func mainHandle(res http.ResponseWriter, req *http.Request) {
 	fmt.Println(s)
 	// отправка клиенту
 	res.Write([]byte(s))
-	// webDir := "web/" + req.URL.Path
-	// mux.Handle("/", http.FileServer(http.Dir(webDir)))
 }
